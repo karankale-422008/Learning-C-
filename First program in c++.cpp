5b8864b212e5d6187860812441bfc84fe57d9fb2
karankale-422008 <karankale422008@gmail.com>
2026-08-09 19:49:07 +0530

    This is my first program of  C++
    This is my first program of  C++ which i have uploaded on github.com

---
+++ First program in c++.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    
+    cout << "This is my first program that will come on github ""\nIt has been one week i have jointed MIT ADT University ";
+    cout << "\nNow i have started learning about C++ from code with harry ""\nNow we will see what i will do in future. ";
+    cout <<"\nI want to see myself as an entrepreneur";
+    
+    return 0;
+}
