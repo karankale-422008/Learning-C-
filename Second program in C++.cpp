1f30c5b58f6db3546f4739e20138fd0192577323
Karan Kale <karankale422008@gmail.com>
2026-08-10 16:56:22 +0530

    This is my second program in c++
    This is my second program in c++

---
+++ Second program in C++.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+    
+    int      karan_age          = 18      ;
+    float    karan_height       = 1.72    ;
+    char     karan_short_name   = 'k'     ;
+    double   karan_run_time     = 30.7268 ;
+    bool     karan_study        = true    ;
+    
+    cout <<   "karan's age is                   := "<< karan_age         << ""       ;
+    cout << "\nkaran's height is                := "<< karan_height      << " Meter" ;
+    cout << "\nkaran's name first letter is     := "<< karan_short_name  << ""       ;
+    cout << "\nkaran 100 meter run time is      := "<< karan_run_time    << " Sec"   ;
+    cout << "\nkaran is good in his studies     := "<< karan_study       << ""       ; 
+
+    return 0;
+}
