5d3ab646fc70794f0ee29fd402dec19eec72c811
Karan Kale <karankale422008@gmail.com>
2026-08-11 22:25:59 +0530

    This is my third program in C++
    This program is all about input and output
    
    We are taking two numbers as input and
    Giving there sum as an output

---
+++ Third program in C++.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    
+ int Number_1 ;
+ int Number_2 ;
+ 
+ cout    <<            "Enter Number 1 \n "  ;
+ cin     >> Number_1                         ;
+ cout    <<            "Enter Number 2 \n "  ;
+ cin     >> Number_2                         ; 
+ cout    <<             Number_1+Number_2    ;
+    return 0;
+}
